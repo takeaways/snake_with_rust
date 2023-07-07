c179f4b7447931aea2df0ac6a7d2f49537ae7fef
takeaways <wkdrjsdlf2@gmail.com>
2023-07-07 23:51:02 +0900

    wasm 쪽으로 함수 넘기고 실행 시키는 방법

--- www/index.js
+++ www/index.js
@@ -1,8 +1,19 @@
 const init = async () => {
-  const response = await fetch("./wasm/sum.wasm");
+  const importObject = {
+    console: {
+      log: () => {
+        console.log("Just Logging Something!");
+      },
+      error: () => {
+        console.error("Just Logging Something!");
+      },
+    },
+  };
+
+  const response = await fetch("./wasm/sumWithLog.wasm");
   const buffer = await response.arrayBuffer();
-  const wasm = await WebAssembly.instantiate(buffer);
-  const add = wasm.instance.exports.add;
-  console.log(add(10, 20));
+  const wasm = await WebAssembly.instantiate(buffer, importObject);
+  const sum = wasm.instance.exports.sum;
+  console.log(sum(10, 20));
 };
 init();
