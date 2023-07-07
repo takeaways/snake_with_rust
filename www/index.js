83715309d2be7d4276a52ebb849db7490bff8313
takeaways <wkdrjsdlf2@gmail.com>
2023-07-08 00:29:25 +0900

    js에서 메모리 만들어서 -> wasm으로 넘겨서 채우기

--- www/index.js
+++ www/index.js
@@ -1,5 +1,12 @@
 const init = async () => {
+  const memory = new WebAssembly.Memory({
+    initial: 1,
+  });
+
   const importObject = {
+    js: {
+      mem: memory,
+    },
     console: {
       log: () => {
         console.log("Just Logging Something!");
@@ -10,16 +17,20 @@ const init = async () => {
     },
   };
 
-  const response = await fetch("./wasm/sumWithMemory.wasm");
+  const response = await fetch("./wasm/sumWithJsMemory.wasm");
   const buffer = await response.arrayBuffer();
+  debugger;
   const wasm = await WebAssembly.instantiate(buffer, importObject);
+  debugger;
   const sum = wasm.instance.exports.sum;
-  const wasmMemory = wasm.instance.exports.mem;
+  // const wasmMemory = wasm.instance.exports.mem;
+  // const uint8Array = new Uint8Array(wasmMemory.buffer, 0, 2);
+  // const hiText = new TextDecoder().decode(uint8Array);
+  // console.log(`hiText:${hiText}`);
 
-  const uint8Array = new Uint8Array(wasmMemory.buffer, 0, 2);
+  const uint8Array = new Uint8Array(memory.buffer, 0, 2);
   const hiText = new TextDecoder().decode(uint8Array);
   console.log(`hiText:${hiText}`);
-
   console.log(sum(10, 20));
 };
 init();
