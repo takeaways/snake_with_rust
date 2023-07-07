b69411e6ca96b7c7859ae7635a2b67dec9254a82
takeaways <wkdrjsdlf2@gmail.com>
2023-07-07 23:45:58 +0900

    wasm fetch로 사용해보기

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -5,13 +5,16 @@ const path = require("path");
 module.exports = {
   entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "dist"),
     filename: "index.js",
   },
   mode: "development",
   plugins: [
     new CopyPlugin({
-      patterns: [{ from: "wasm", to: "wasm" }],
+      patterns: [
+        { from: "wasm", to: "wasm" },
+        { from: "favicon.ico", to: "favicon.ico" },
+      ],
     }),
     new HtmlWebpackPlugin({
       template: path.relative(__dirname, "index.html"),
